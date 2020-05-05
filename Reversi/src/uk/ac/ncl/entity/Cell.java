8406b37231def2a24d6c2e5f81bf1eda85f3ce25
Ivy <i.kiryakova1@newcastle.ac.uk>
2020-05-05 19:19:43 +0100

    Fixed out of bounds error and double spacing

--- Reversi/src/uk/ac/ncl/entity/Cell.java
+++ Reversi/src/uk/ac/ncl/entity/Cell.java
@@ -128,9 +128,9 @@ public class Cell {
                     d_row += dir[0];
                     d_col += dir[1];
                     temp_score += 1;
-                    if (0 <= d_col &&  d_col < BOARD_SIZE && 0 <=  d_row
+                    if (0 <= d_col &&  d_col < BOARD_SIZE && 0 <= d_row && d_row  < BOARD_SIZE
                             && cells[d_row][d_col].getValue() != CellStatus.EMPTY){
-                        if (cells[d_row][d_col].getValue() != opponent) {
+                        if (cells[d_row][d_col].getValue() == colour) {
                             isLegal = true;
                             score += temp_score;
                             moves.add(new DirectedMove(cells[d_row][d_col], dir));
@@ -142,7 +142,6 @@ public class Cell {
                 }
             }
         }
-
         Move move = new Move(moves, score);
         this.setMove(move);
         return isLegal;
